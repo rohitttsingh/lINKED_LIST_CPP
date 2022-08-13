2fd164c4e6aef816c642dbecc77a44e071873f12
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-08-13 10:58:49 +0530

    Update DSAlearning.cpp

--- DSAlearning.cpp
+++ DSAlearning.cpp
@@ -1,188 +1,178 @@
-#include<bits/stdc++.h>
+#include<c++/v1/bits/stdc++.h>
 
 using namespace std;
 struct Node{
-	int data;
-	 Node* next;
-};		
+    int data;
+     Node* next;
+};
 
- Node *creation(int data){
+ void creation(Node **head,int data){
     Node *newnode = new Node();
     newnode->data=data;
-    newnode->next=NULL;
-    
-    return newnode;
+    newnode->next=*head;
+    *head=newnode;
+
     
 }
 
-Node* insert(Node *head, int data ){
-    if(head==NULL)
-    head = creation (data);
-    
-    else {
-head->next = insert(head->next,data);
-        
-    }
-    
-    return head;
-}
 
 void printingNode( Node * head){
-	 Node * temp = head;
-	while(temp!=NULL){
-		
-		cout<<temp->data<<" ";
-		temp=temp->next;
-	}
+     Node * temp = head;
+    while(temp!=NULL){
+        
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
 }
 void PRINTINGREVERSE(Node * head){
 
 if(head==NULL)
 return;
-	
-	PRINTINGREVERSE(head->next);
-	
-		cout<<head->data<<" ";
+    
+    PRINTINGREVERSE(head->next);
+    
+        cout<<head->data<<" ";
 
 }
 
 void InsertInBetweenAFTER (Node * head , int data, int position){
-	Node * newNode = new Node();
-	newNode->data=data;
-	Node * temp =head;
+    Node * newNode = new Node();
+    newNode->data=data;
+    Node * temp =head;
 
 
-	while(temp!=NULL){
-		if(temp->data==position){
-			newNode->next=temp->next;
-			temp->next=newNode;
-			return;
-		}
-		
-		
-		temp=temp->next;
-	}
-	
+    while(temp!=NULL){
+        if(temp->data==position){
+            newNode->next=temp->next;
+            temp->next=newNode;
+            return;
+        }
+        
+        
+        temp=temp->next;
+    }
+    
 }
 
 void insertinBetweenBefore (Node * head , int data, int position){
-	Node * newNode = new Node();
-	newNode->data=data;
-	Node * temp =head;
+    Node * newNode = new Node();
+    newNode->data=data;
+    Node * temp =head;
 
-	while(temp!=NULL){
-		if(temp->next->data==position){
+    while(temp!=NULL){
+        if(temp->next->data==position){
 
-			newNode->next=temp->next;
-			temp->next=newNode;
-			return;
-		}
-		
-		temp=temp->next;
-	}
+            newNode->next=temp->next;
+            temp->next=newNode;
+            return;
+        }
+        
+        temp=temp->next;
+    }
 }
 void insertatend(Node *head,int data){
-	Node * newNode = new Node();
-	newNode->data=data;
-	Node * temp =head;
-	
-	while(temp!=NULL){
-		if(temp->next==NULL){
-			newNode->next=NULL;
-			temp->next=newNode;
-			
-			return;
-		}
-		temp=temp->next;
-	}
-	
+    Node * newNode = new Node();
+    newNode->data=data;
+    Node * temp =head;
+    
+    while(temp!=NULL){
+        if(temp->next==NULL){
+            newNode->next=NULL;
+            temp->next=newNode;
+            
+            return;
+        }
+        temp=temp->next;
+    }
+    
 }
 void deletenodeafter(Node *head,int position){
-	Node * temp =head;
-	
-	while(temp!=NULL){
-		if(temp->data==position){
-			
-			free(temp->next);
-			temp->next=temp->next->next;
-			
-			return ;
-		}
-		temp=temp->next;
-	}
-	
+    Node * temp =head;
+    
+    while(temp!=NULL){
+        if(temp->data==position){
+            
+            free(temp->next);
+            temp->next=temp->next->next;
+            
+            return ;
+        }
+        temp=temp->next;
+    }
+    
 }
 void deletenodebefore(Node *head,int position){
-	Node * temp =head;
-	
-	while(temp!=NULL){
-		if(temp->next->next->data==position){
-			
-			free(temp->next);
-			temp->next=temp->next->next;
-			
-			return ;
-		}
-		temp=temp->next;
-	}
-	
+    Node * temp =head;
+    
+    while(temp!=NULL){
+        if(temp->next->next->data==position){
+            
+            free(temp->next);
+            temp->next=temp->next->next;
+            
+            return ;
+        }
+        temp=temp->next;
+    }
+    
 }
 void deletenodeatend(Node *head){
 
-	Node * temp =head;
-	
-	while(temp!=NULL){
-		if(temp->next->next==NULL){
-			
-			free(temp->next);
-			temp->next=NULL;
-			
-			return;
-		}
-		temp=temp->next;
-	}
-	
+    Node * temp =head;
+    
+    while(temp!=NULL){
+        if(temp->next->next==NULL){
+            
+            free(temp->next);
+            temp->next=NULL;
+            
+            return;
+        }
+        temp=temp->next;
+    }
+    
 }
 
 int main()
-{ 
-	
+
+{
+    
 Node* head = NULL;
 
-int a[]={213,21,3,3142,344,464,2,23,56};
+int a[] = {213,21,3,3142,8,344,464,2,23,56};
 
 for(auto i:a)
-head=insert(head,i);
+creation(&head,i);
 
 //PRINTING ORIGINAL LL  " LL = lINKED LIST"
 cout<<"Original Linked List \n";
 printingNode(head);
 
-//INSERT AFTER A NODE 
+//INSERT AFTER A NODE
 InsertInBetweenAFTER(head,9,8);
 cout<<"\n";
 cout<<"Inserting 9 Node After 8 \n";
 printingNode(head);
 
-//INSERT BEFORE A NODE 
+//INSERT BEFORE A NODE
 insertinBetweenBefore(head,10,8);
 cout<<"\n";
 cout<<"Inserting 10 Node Before 8 \n";
 printingNode(head);
 
-//INSERT AT THE END 
+//INSERT AT THE END
 insertatend(head,11);
 cout<<"\n";
 cout<<"Inserting 11 Node at End \n";
 printingNode(head);
 
-//DELETE AFTER A NODE 
+//DELETE AFTER A NODE
 deletenodeafter(head,8);
 cout<<"\n";
 cout<<"Delete 9 Node after 8 \n";
 printingNode(head);
 
-//DELETE BEFORE A NODE 
+//DELETE BEFORE A NODE
 deletenodebefore(head,8);
 cout<<"\n";
 cout<<"Delete 10 Node before 8 \n";
@@ -199,4 +189,5 @@ cout<<"\n";
 cout<<"Print in Reverse \n";
 PRINTINGREVERSE(head);
 return 0;
+    
 }
